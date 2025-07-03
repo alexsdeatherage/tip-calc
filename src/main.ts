c0a57c6f676a1771a28802e38748c37d141b93e9
alexsdeatherage <alexdeatherage4829@gmail.com>
2025-07-02 23:50:27 -0700

    Add Tip Calc Per Person

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-function calculateTip(bill: number, tipPercent: number): number {
+function calculateTotalTip(bill: number, tipPercent: number): number {
     let tipAmount: number
 
     if (bill > 0 && tipPercent > 0) {
@@ -12,16 +12,22 @@ function calculateTip(bill: number, tipPercent: number): number {
 const billInput = document.getElementById('bill') as HTMLInputElement;
 const tipInput = document.getElementById('tip') as HTMLInputElement;
 const calcButton = document.getElementById('calc') as HTMLButtonElement;
-const resultDisplay = document.getElementById('result') as HTMLParagraphElement;
+const totalTipDisplay = document.getElementById('result') as HTMLParagraphElement;
+const tipPerPersonDisplay = document.getElementById('tip-amount') as HTMLParagraphElement;
+const peopleInput = document.getElementById('people') as HTMLInputElement;
 
 calcButton.addEventListener('click', () => {
     const bill = parseFloat(billInput.value);
     const tipPercent = parseFloat(tipInput.value);
 
     if (bill > 0 && tipPercent > 0) {
-        const tip = calculateTip(bill, tipPercent)
-        resultDisplay.textContent = `Tip Amount: $${tip.toFixed(2)}`;
+        const tip = calculateTotalTip(bill, tipPercent)
+        const people = parseInt(peopleInput.value)
+        const tipPerPerson = tip / people
+        totalTipDisplay.textContent = `Tip Amount: $${tip.toFixed(2)}`;
+        tipPerPersonDisplay.textContent = tipPerPerson.toString();
+
     } else {
-        resultDisplay.textContent = `Please enter valid values for bill and tip percentage`
+        totalTipDisplay.textContent = `Please enter valid values for bill and tip percentage`
     }
 });
